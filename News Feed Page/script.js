1c6f0793dfe7fb49a84cca3a51c702db1740a2f3
Wondong <ehdgus5500@gmail.com>
2020-07-26 21:12:11 +0900

    sidebar dark mode

--- News Feed Page/script.js
+++ News Feed Page/script.js
@@ -12,6 +12,9 @@ const sidebar = document.querySelector('.sidebar');
 const sidebarWrapper = document.querySelector('.sidebar-wrapper')
 const xBtn = document.querySelector('.sidebar-header i');
 
+const toggle = document.querySelector('.toggle');
+const circle = document.querySelector('.circle');
+
 // Post modal
 postBtn.addEventListener('click', () => {
     modal.style.display  = 'block';
@@ -58,3 +61,24 @@ xBtn.addEventListener('click', () => {
     sidebarWrapper.classList.remove('sidebar-wrapper-display')
 })
 
+// dark mode
+const darkElements1 = document.querySelectorAll('.dark-mode-1');
+const darkElements2 = document.querySelectorAll('.dark-mode-2');
+const lightTexts = document.querySelectorAll('.light-text');
+const borders = document.querySelectorAll('.border');
+
+toggle.addEventListener('click', () => {
+    circle.classList.toggle('move');
+    darkElements1.forEach((el) => {
+        el.classList.toggle('dark-1');
+    });
+    darkElements2.forEach((el) => {
+        el.classList.toggle('dark-2');
+    })
+    lightTexts.forEach((el) => {
+        el.classList.toggle('light');
+    })
+    borders.forEach((el) => {
+        el.classList.toggle('border-color')
+    })
+})
